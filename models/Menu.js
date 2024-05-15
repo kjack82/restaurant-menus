f531e96eaf59a317c4ea88a4bdc21e8935872ebb
kjack82 <kprothero1702@gmail.com>
2024-05-15 16:29:26 +0100

    up to testing

--- models/Menu.js
+++ models/Menu.js
@@ -1,6 +1,8 @@
 const {sequelize} = require('../db');
-const { Sequelize } = require('sequelize');
+const { Sequelize, DataTypes } = require('sequelize');
 
-// TODO - create a Menu model
+const Menu = sequelize.define("menu", {
+    title: DataTypes.STRING
+})
 
 module.exports = {Menu};
--- models/Restaurant.js
+++ models/Restaurant.js
@@ -1,6 +1,10 @@
 const {sequelize} = require('../db');
 const { Sequelize } = require('sequelize');
 
-// TODO - create a Restaurant model
+const Restaurant = sequelize.define("restaurant", {
+    name: DataTypes.STRING,
+    location: DataTypes.STRING,
+    cuisine: DataTypes.STRING
+})
 
 module.exports = {Restaurant};
